a31b3e20b421145ef8c2d1e190cc48d0dd783518
Phil Wang <lucidrains@gmail.com>
2021-04-18 14:31:29 -0700

    remove einops

--- local_attention/rotary.py
+++ local_attention/rotary.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn, einsum
-from einops import rearrange, repeat
 
 class SinusoidalEmbeddings(nn.Module):
     def __init__(self, dim):
@@ -11,19 +10,15 @@ class SinusoidalEmbeddings(nn.Module):
     def forward(self, x):
         n = x.shape[-2]
         t = torch.arange(n, device = x.device).type_as(self.inv_freq)
-        sinusoid_inp = torch.einsum('i , j -> i j', t, self.inv_freq)
-        emb = torch.cat((sinusoid_inp.sin(), sinusoid_inp.cos()), dim=-1)
+        freqs = torch.einsum('i , j -> i j', t, self.inv_freq)
+        emb = torch.cat((freqs, freqs), dim=-1)
         return emb[None, :, :]
 
-def rotate_every_two(x):
-    x = rearrange(x, '... (d j) -> ... d j', j = 2)
-    x1, x2 = x.unbind(dim = -1)
-    x = torch.stack((-x2, x1), dim = -1)
-    return rearrange(x, '... d j -> ... (d j)')
+def rotate_half(x):
+    x = x.reshape((x.shape[0], -1, 2, x.shape[-1] // 2))
+    x1, x2 = x.unbind(dim = -2)
+    return torch.cat((-x2, x1), dim = -1)
 
-def apply_rotary_pos_emb(q, k, sinu_pos):
-    sinu_pos = rearrange(sinu_pos, '() n (j d) -> n j d', j = 2)
-    sin, cos = sinu_pos.unbind(dim = -2)
-    sin, cos = map(lambda t: repeat(t, 'b n -> b (n j)', j = 2), (sin, cos))
-    q, k = map(lambda t: (t * cos) + (rotate_every_two(t) * sin), (q, k))
+def apply_rotary_pos_emb(q, k, freqs):
+    q, k = map(lambda t: (t * freqs.cos()) + (rotate_half(t) * freqs.sin()), (q, k))
     return q, k
