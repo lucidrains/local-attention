2d204cc65849fd104985c9ab5a518af9d4930a27
Phil Wang <lucidrains@gmail.com>
2022-12-08 15:57:43 -0800

    expose local multi-head attention, for use in audiolm

--- local_attention/__init__.py
+++ local_attention/__init__.py
@@ -1,2 +1,2 @@
 from local_attention.local_attention import LocalAttention
-from local_attention.transformer import LocalTransformer
+from local_attention.transformer import LocalTransformer, LocalMHA
