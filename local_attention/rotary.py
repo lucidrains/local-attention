1f2507212742cd00453419e2b9c7512293a49982
lucidrains <lucidrains@gmail.com>
2024-04-20 12:32:25 -0700

    turn off autocast within rotary embed forward + applying

--- local_attention/rotary.py
+++ local_attention/rotary.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn, einsum
+from torch.cuda.amp import autocast
 
 from einops import rearrange
 
@@ -27,6 +28,7 @@ class SinusoidalEmbeddings(nn.Module):
         scale = (torch.arange(0, dim, 2) + 0.4 * dim) / (1.4 * dim)
         self.register_buffer('scale', scale, persistent = False)
 
+    @autocast(enabled = False)
     def forward(self, x):
         seq_len, device = x.shape[-2], x.device
 
@@ -48,6 +50,7 @@ def rotate_half(x):
     x1, x2 = x.unbind(dim = -2)
     return torch.cat((-x2, x1), dim = -1)
 
+@autocast(enabled = False)
 def apply_rotary_pos_emb(q, k, freqs, scale = 1):
     q_len = q.shape[-2]
     q_freqs = freqs[..., -q_len:, :]
