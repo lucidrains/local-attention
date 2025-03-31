64bbbc7f60e11e3f812cf08ec8794c2d231b39f8
SimonCao1207 <hainamkaist@gmail.com>
2025-03-31 23:23:13 +0900

    update

--- train.py
+++ train.py
@@ -1,3 +1,20 @@
+"""
+This training script can be run both on a single gpu in debug mode,
+and also in a larger training run with distributed data parallel (ddp).
+
+To run on a single GPU, example:
+$ python train.py 
+
+To run with DDP on 4 gpus on 1 node, example:
+$ torchrun --standalone --nproc_per_node=4 train.py
+
+To run with DDP on 4 gpus across 2 nodes, example:
+- Run on the first (master) node with example IP 123.456.123.456:
+$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr=123.456.123.456 --master_port=1234 train.py
+- Run on the worker node:
+$ torchrun --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr=123.456.123.456 --master_port=1234 train.py
+"""
+
 import random
 import tqdm
 import gzip
@@ -17,13 +34,16 @@ from local_attention import LocalTransformer
 
 NUM_BATCHES = int(1e5)
 BATCH_SIZE = 4
-GRADIENT_ACCUMULATE_EVERY = 4
+GRADIENT_ACCUMULATE_EVERY = 5*4
 LEARNING_RATE = 2e-4
 VALIDATE_EVERY  = 100
 GENERATE_EVERY  = 500
 GENERATE_LENGTH = 2048
 SEQ_LEN = 2048
-MIN_GPU_MEMORY = 20 * 1024**3  # 20GB in bytes
+LOG_INTERVAL = 1
+DEVICE = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc.
+BACKEND = 'nccl' # DDP settings
+
 # helpers
 
 def cycle(loader):
@@ -37,28 +57,28 @@ def decode_token(token):
 def decode_tokens(tokens):
     return ''.join(list(map(decode_token, tokens)))
 
-def check_gpu_memory(min_memory_required):
-    """
-    Check if the current GPU has enough free memory.
-    """
-    local_rank = int(os.environ["LOCAL_RANK"])
-    free_memory = torch.cuda.mem_get_info(local_rank)[0]  # Free memory in bytes
-    return free_memory >= min_memory_required
-
-def setup_distributed():
+ddp = int(os.environ.get('RANK', -1)) != -1 # is this a ddp run?
+if ddp:
     # initialize distributed training
+    dist.init_process_group(backend=BACKEND)
+    ddp_rank = int(os.environ['RANK'])
+    ddp_local_rank = int(os.environ['LOCAL_RANK'])
+    ddp_world_size = int(os.environ['WORLD_SIZE'])
+    DEVICE = f'cuda:{ddp_local_rank}'
+    torch.cuda.set_device(DEVICE)
+    master_process = ddp_rank == 0 # this process will do logging, checkpointing, loading data etc.
+    seed_offset = ddp_rank # each process gets a different seed
 
-    dist.init_process_group(backend='nccl')
-    local_rank = dist.get_rank()
-    torch.cuda.set_device(local_rank)
+    assert  GRADIENT_ACCUMULATE_EVERY % ddp_world_size == 0
+    GRADIENT_ACCUMULATE_EVERY //= ddp_world_size
+ 
+else:
+    # if not ddp, we are running on a single gpu, and one process
+    master_process = True
+    seed_offset = 0
+    ddp_world_size = 1
 
-     # Check GPU memory
-    if not check_gpu_memory(MIN_GPU_MEMORY):
-        print(f"GPU {local_rank} does not have enough memory. Skipping...")
-        dist.destroy_process_group()
-        exit(0)
-
-setup_distributed()
+torch.manual_seed(1337 + seed_offset)
 
 # instantiate GPT-like decoder model
 model = LocalTransformer(
@@ -69,33 +89,31 @@ model = LocalTransformer(
     local_attn_window_size = 256,
     max_seq_len = SEQ_LEN,
     use_dynamic_pos_bias = True
-).cuda()
+).to(DEVICE)
 
 # wrap model for distributed training
-local_rank = int(os.environ["LOCAL_RANK"])
-model = DDP(model, device_ids=[local_rank])
+if ddp:
+    model = DDP(model, device_ids=[ddp_local_rank])
+raw_model = model.module if ddp else model # unwrap DDP container if needed
 
 # prepare enwik8 data
 
-if local_rank == 0:
+if master_process:
     with gzip.open('./data/enwik8.gz') as file:
-        print(f"[Process {local_rank}] Unzipping enwik8.gz...")
+        print("[Process 0] Unzipping enwik8.gz...")
         X = np.frombuffer(file.read(int(95e6)), dtype=np.uint8)
         trX, vaX = np.split(X, [int(90e6)])
-        trX, vaX = torch.from_numpy(trX).cuda(), torch.from_numpy(vaX).cuda()
+        trX = torch.from_numpy(np.copy(trX)).to(DEVICE)
+        vaX = torch.from_numpy(np.copy(vaX)).to(DEVICE)
 else:
-    trX = torch.empty(int(90e6), dtype=torch.uint8).cuda()
-    vaX = torch.empty(int(5e6), dtype=torch.uint8).cuda()
+    trX = torch.empty(int(90e6), dtype=torch.uint8).to(DEVICE)
+    vaX = torch.empty(int(5e6), dtype=torch.uint8).to(DEVICE)
 
-    print(f"[Process {local_rank}] Waiting for data loading to complete...")
-
-# Synchronize all processes
-dist.barrier()
-dist.broadcast(trX, src=0)
-dist.broadcast(vaX, src=0)
-
-if local_rank != 0:
-    print(f"[Process {local_rank}] Data broadcasted to all processes.")
+if ddp:
+    # Synchronize all processes
+    dist.barrier()
+    dist.broadcast(trX, src=0)
+    dist.broadcast(vaX, src=0)
 
 data_train, data_val = trX, vaX
 class TextSamplerDataset(Dataset):
@@ -115,12 +133,17 @@ class TextSamplerDataset(Dataset):
 train_dataset = TextSamplerDataset(data_train, SEQ_LEN)
 val_dataset   = TextSamplerDataset(data_val, SEQ_LEN)
 
-# create distributed data loaders
-train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset)
+if ddp:
+    # create distributed data loaders
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset)
 
-train_loader  = cycle(DataLoader(train_dataset, batch_size = BATCH_SIZE, sampler=train_sampler))
-val_loader    = cycle(DataLoader(val_dataset, batch_size = BATCH_SIZE, sampler=val_sampler))
+    train_loader = cycle(DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler))
+    val_loader = cycle(DataLoader(val_dataset, batch_size=BATCH_SIZE, sampler=val_sampler))
+else:
+    # create standard data loaders for single GPU or CPU
+    train_loader = cycle(DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True))
+    val_loader = cycle(DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False))
 
 # optimizer
 
@@ -128,30 +151,34 @@ optim = Adam(model.parameters(), lr=LEARNING_RATE)
 
 # training
 
-for i in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
+for iter_num in tqdm.tqdm(range(NUM_BATCHES), mininterval=10., desc='training'):
     model.train()
 
-    for __ in range(GRADIENT_ACCUMULATE_EVERY):
+    for micro_step in range(GRADIENT_ACCUMULATE_EVERY):
+        if ddp:
+            model.require_backward_grad_sync = (micro_step == GRADIENT_ACCUMULATE_EVERY - 1)
         loss = model(next(train_loader), return_loss = True)
         loss.backward()
 
-    print(f'Process {local_rank}: training loss: {loss.item()}')
+    if iter_num % LOG_INTERVAL == 0 and master_process:
+        print(f"iter {iter_num}: loss {loss.item():.4f}")
+
     torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
     optim.step()
     optim.zero_grad()
 
-    if i % VALIDATE_EVERY == 0:
+    if iter_num % VALIDATE_EVERY == 0 and master_process:
         model.eval()
         with torch.no_grad():
             loss = model(next(val_loader), return_loss = True)
             print(f'validation loss: {loss.item()}')
 
-    if i % GENERATE_EVERY == 0:
+    if iter_num % GENERATE_EVERY == 0 and master_process:
         model.eval()
         inp = random.choice(val_dataset)[:-1]
         prime = decode_tokens(inp)
-        print(f'%s \n\n %s', (prime, '*' * 100))
+        print(f"{prime} \n\n {'*'*100}")
 
-        sample = model.module.generate(inp[None, ...], GENERATE_LENGTH)
+        sample = raw_model.generate(inp[None, ...], GENERATE_LENGTH)
         output_str = decode_tokens(sample[0])
-        print(output_str)
+        print(f"{output_str} \n\n {'*'*100}")
